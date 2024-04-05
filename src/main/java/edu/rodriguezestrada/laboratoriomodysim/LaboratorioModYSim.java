e805bd7be3f364b56102d04c1deef9986503c775
rafastrada <guillerafael98@gmail.com>
2024-04-05 18:09:05 -0300

    Inicio Repositorio
    Proyecto en NetBeans

---
+++ src/main/java/edu/rodriguezestrada/laboratoriomodysim/LaboratorioModYSim.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.rodriguezestrada.laboratoriomodysim;
+
+/**
+ *
+ * @author gestrada
+ */
+public class LaboratorioModYSim {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
