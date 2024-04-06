9d2c560a3eaf1afc0eaf34202e74fdf4904e19c9
rafastrada <guillerafael98@gmail.com>
2024-04-05 21:55:21 -0300

    Comparador de eventos agregado

--- src/main/java/edu/rodriguezestrada/laboratoriomodysim/simulacion/eventos/Evento.java
+++ src/main/java/edu/rodriguezestrada/laboratoriomodysim/simulacion/eventos/Evento.java
@@ -2,12 +2,13 @@ package edu.rodriguezestrada.laboratoriomodysim.simulacion.eventos;
 
 import edu.rodriguezestrada.laboratoriomodysim.simulacion.Avion;
 import edu.rodriguezestrada.laboratoriomodysim.simulacion.probabilidad.Probabilidad;
+import java.util.Comparator;
 
 /**
  *
  * @author gestrada
  */
-public abstract class Evento {
+public abstract class Evento implements Comparator {
 
     private static Probabilidad valoresAzarosos = null;
 
@@ -52,6 +53,25 @@ public abstract class Evento {
      */
     public Avion getEntidad() {
         return entidad;
+    }
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        int salida = 1;
+        Evento evento1 = (Evento) o1, evento2 = (Evento) o2;
+        
+        if (evento1.getTiempo() < evento2.getTiempo()) {
+            salida = -1;
+        }
+        else {
+            if (evento1.getTiempo() == evento2.getTiempo()
+                    &&
+                    evento1.getClass().getSimpleName().equals("Salida")) {
+                salida = -1;
+            }
+        }
+        
+        return salida;
     }
 
 }
