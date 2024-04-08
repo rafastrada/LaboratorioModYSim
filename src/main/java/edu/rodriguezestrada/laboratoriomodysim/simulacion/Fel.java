b623ba1efdaa42dd97611edcfd4b639e4ac469d3
Pablo Rodriguez Celiz <rodriguezpabl57@gmail.com>
2024-04-08 00:52:40 -0300

    Agregacion metodo de ordenamiento en la clase fel

--- src/main/java/edu/rodriguezestrada/laboratoriomodysim/simulacion/Fel.java
+++ src/main/java/edu/rodriguezestrada/laboratoriomodysim/simulacion/Fel.java
@@ -2,6 +2,7 @@ package edu.rodriguezestrada.laboratoriomodysim.simulacion;
 
 import edu.rodriguezestrada.laboratoriomodysim.simulacion.eventos.Evento;
 import edu.rodriguezestrada.laboratoriomodysim.simulacion.eventos.Fin;
+import edu.rodriguezestrada.laboratoriomodysim.simulacion.eventos.Salida;
 import java.util.ArrayList;
 
 /**
@@ -15,4 +16,22 @@ public class Fel extends ArrayList<Evento> {
         // se inicia la fel con el evento 'Fin' incluido
         this.add(new Fin(tiempoFinalizacion));
     }
+    
+    public void ordenarFEL() {
+        int n = this.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                Evento event1 = this.get(j);
+                Evento event2 = this.get(j + 1);
+                if (event1.getTiempo() > event2.getTiempo() ||
+                        (event1.getTiempo() == event2.getTiempo() &&
+                                event1.getClass().equals(Salida.class))) {
+                    this.set(j, event2);
+                    this.set(j + 1, event1);
+                }
+            }
+        }
+    }
 }
+    
+
